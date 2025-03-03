b122bd6dc836ee0e91729e4d0b4f2a9da4c4f47c
PedroSantana23 <ph.silva.santana@gmail.com>
2025-03-03 20:29:33 -0300

    Seção 27: Node.js -> finalizada

---
+++ javascript/secao-10-dom-selecionando-elemento/002-query-selector/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const h1 = document.querySelector("h1");
+const classP = document.querySelector(".paragrafo");
+const idP = document.querySelector("#paragrafo");
+const strong = document.querySelector(".paragrafo-strong")
+
+console.log(h1);
+console.log(classP);
+console.log(idP);
+console.log(strong)
+
